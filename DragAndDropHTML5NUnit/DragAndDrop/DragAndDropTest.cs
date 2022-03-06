5e1f919d140dda7b6afcab5d91c310a4be032d3e
de2010rht <de.rht2010@gmail.com>
2022-03-06 16:55:09 +0530

    New project on Drag and Drop for HTML5 based application

---
+++ DragAndDropHTML5NUnit/DragAndDrop/DragAndDropTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace DragAndDrop
+{
+    [TestFixture]
+    public class DragAndDropTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [Test]
+        public void DragAndDropTestMethod()
+        {
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/drag_and_drop");
+            IWebElement source = wait.Until(d => d.FindElement(By.Id("column-a")));
+
+            var jsFile = File.ReadAllText(@"D:\My Works\workspaceVS\DragAndDropHTML5NUnit\drag_and_drop_helper.js");
+            IJavaScriptExecutor jsExecutor = driver as IJavaScriptExecutor;
+            jsExecutor.ExecuteScript(jsFile + "$('#column-a').simulateDragDrop({dropTarget: '#column-b'});");
+
+            Assert.AreEqual("A", driver.FindElement(By.XPath("//*[@id='column-b']/header")).Text);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver.Manage().Window.Maximize();
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ DragAndDropHTML5NUnit/DragAndDrop/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DragAndDrop")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DragAndDrop")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0d43f6cf-843e-4bbb-ab18-f1d3a9a54385")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
